b3036cdc05aae51802ccee8a9a8c706d49d9e258
Petr Nikolaev <pitnikola@gmail.com>
2020-02-21 06:01:32 -0800

    Initial Commit

---
+++ NonActivatingPanelIssue/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  NonActivatingPanelIssue
+//
+//  Created by Petr Nikolaev on 21/02/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ NonActivatingPanelIssue/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NonActivatingPanelIssue
+//
+//  Created by Petr Nikolaev on 21/02/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
